7a45a73d15b136851ebedb1c7510b0e2c5f6591b
samsonajulor <s.ajulor@abbeymortgagebank.com>
2022-07-29 12:25:26 +0100

    added the comments service

---
+++ node apps/blogger/server/comments/index.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import { randomBytes } from 'crypto';
+import pkg from 'body-parser';
+
+const { json } = pkg;
+
+const app = express();
+app.use(json());
+
+const commentsByPostId = {};
+
+app.get('/posts/:id/comments', (req, res) => {
+  res.send(commentsByPostId[req.params.id] || []);
+});
+
+app.post('/posts/:id/comments', (req, res) => {
+  const commentId = randomBytes(4).toString('hex');
+  const { content } = req.body;
+
+  const comments = commentsByPostId[req.params.id] || [];
+
+  comments.push({ id: commentId, content });
+
+  commentsByPostId[req.params.id] = comments;
+
+  res.status(201).send(comments);
+});
+
+app.listen(4001, () => {
+  console.log('Listening on 4001');
+});
