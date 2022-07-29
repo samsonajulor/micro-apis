138ba8b500fdfa43edbe03d73500399df3bc0f68
samsonajulor <s.ajulor@abbeymortgagebank.com>
2022-07-29 15:51:34 +0100

    added the event bus server

---
+++ node apps/blogger/server/event-bus/index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import bodyParser from "body-parser";
+import axios from "axios";
+
+const { json } = bodyParser;
+
+const app = express();
+app.use(json());
+
+app.post("/events", (req, res) => {
+  const event = req.body;
+
+  axios.post("http://localhost:4000/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  axios.post("http://localhost:4001/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  axios.post("http://localhost:4002/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  res.send({ status: "OK" });
+});
+
+app.listen(4005, () => {
+  console.log("Listening on 4005");
+});
