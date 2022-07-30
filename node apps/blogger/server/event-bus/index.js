46f5904af25d1c88e89c88f5370011915b669d68
samsonajulor <s.ajulor@abbeymortgagebank.com>
2022-07-31 00:32:35 +0100

    syncing communications between posts and event bus completed

--- node apps/blogger/server/event-bus/index.js
+++ node apps/blogger/server/event-bus/index.js
@@ -28,7 +28,7 @@ app.post("/events", (req, res) => {
 
   // send event to the moderation service
   axios.post('http://localhost:4002/events', event).catch((err) => {
-    console.log(err.message);
+    console.log(err.message, 'error from the moderation event');
   });
 
   // send event to the query service
