caa4a6c090bed1bbf0d972371c3d59215a12d1bf
samsonajulor <s.ajulor@abbeymortgagebank.com>
2022-07-30 05:42:27 +0100

    comment moderation completed for the server

--- node apps/blogger/server/comments/index.js
+++ node apps/blogger/server/comments/index.js
@@ -39,7 +39,7 @@ app.post('/posts/:id/comments', async (req, res) => {
   res.status(201).send(comments);
 });
 
-app.post("/events", (req, res) => {
+app.post("/events", async (req, res) => {
   console.log("Event Received:", req.body.type);
 
   const { type, data } = req.body;
--- node apps/blogger/server/query/index.js
+++ node apps/blogger/server/query/index.js
@@ -28,6 +28,18 @@ app.post('/events', (req, res) => {
     const post = posts[postId];
     post.comments.push({ id, content, status });
   }
+  
+  if (type === 'CommentUpdated') {
+    const { id, content, postId, status } = data;
+
+    const post = posts[postId];
+    const comment = post.comments.find((comment) => {
+      return comment.id === id;
+    });
+
+    comment.status = status;
+    comment.content = content;
+  }
 
   console.log(posts);
 
