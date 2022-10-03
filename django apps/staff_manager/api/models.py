d784dd2d11f50215fe0361ef63f5668cb9a3b1e1
samsonajulor <s.ajulor@abbeymortgagebank.com>
2022-10-03 04:32:15 +0100

    completed a django application

--- django apps/staff_manager/api/models.py
+++ django apps/staff_manager/api/models.py
@@ -4,10 +4,6 @@ from django.db import models
 
 class Staff(models.Model):
     fullname = models.CharField(max_length=100)
-    emp_code = models.CharField(max_length=3)
+    staff_code = models.CharField(max_length=3)
     mobile = models.CharField(max_length=15)
 
-    # Create / Insert / Add - POST
-    # Retrieve /  Fetch - GET
-    # Update / Edit - PUT
-    # Delete / Remove - DELETE
